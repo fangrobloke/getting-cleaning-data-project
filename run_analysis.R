53cb973a2d2cb806aa3e5bf55917c42f0ab1a98c
fangrobloke <fang.robloke@gmail.com>
2016-09-11 14:51:09 +0700

    add analysis

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+filesPath <- "C:/Users/Wutdy/Desktop/datasci/getting_cleaning/project"
+setwd(filesPath)
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+
